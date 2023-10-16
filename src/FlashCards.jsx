a030b7d6d0fedeb01546ac6bf25db3c9b92dc48c
Robert <pinneyr@hotmail.com>
2023-10-16 11:38:50 +0200

    first commit

---
+++ src/FlashCards.jsx
@@ -0,0 +1,27 @@
+import { questions } from "./data-questions";
+import "./styles.css";
+import { useState } from "react";
+
+export default function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? "selected" : ""}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
